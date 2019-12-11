766043c89976fd03e29bc560788977cd58ff43d7
Alexandra Boyko <Alexboyko04@gmail.com>
2019-12-11 14:36:13 +0200

    Updates

---
+++ precache-manifest.7c9b1f771e670ba3bf94eb1021dc1f0d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6cba1703033f449f77244ffb355aac76",
+    "url": "/react_add-todo-form/index.html"
+  },
+  {
+    "revision": "e14e621c10ce6a2e8bac",
+    "url": "/react_add-todo-form/static/css/main.cb9e985a.chunk.css"
+  },
+  {
+    "revision": "b829692f51f58c98b1bd",
+    "url": "/react_add-todo-form/static/js/2.d29b3332.chunk.js"
+  },
+  {
+    "revision": "e14e621c10ce6a2e8bac",
+    "url": "/react_add-todo-form/static/js/main.8e3e46d8.chunk.js"
+  },
+  {
+    "revision": "4953b3d381bec0cda918",
+    "url": "/react_add-todo-form/static/js/runtime~main.9d188fa2.js"
+  }
+]);
